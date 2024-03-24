fd23d0d88419a1f76c51b897cbf3551d7c18a1f7
wheatleyhilldental <wheatleyhilldental@gmail.com>
2024-03-24 17:49:05 +0000

    update sentence

--- src/Pages/PagesData/AboutData.js
+++ src/Pages/PagesData/AboutData.js
@@ -1,12 +1,18 @@
 const about_long_des = {
   text: `We are a passionate and steadfast team of 10. Together, we believe in one success formula - Best Service Delivery, every time.
 
-  A beautiful smile, takes one, extra mile;
-  and we deliver the smile;
-  Best smile service hence is a must;
+  A beautiful smile takes one extra mile;
+  and we deliver the smile
+
+  We look forward to providing you and your family with the dental excellence and care you deserve.
+
+  We have extensive expereince from Preventive care to Restorative and Cosmetic solutions for all your dental issues.
+
+
+  Best smile service hence is a must. 
   we have the proof, it is your trust.
   
-  Your Wheatley Hill Dental Surgery is now preparing for its centenary celebration. Yes, it is completing its 100 years of service soon and we owe all our success to the trust put in by you.`,
+  Your Wheatley Hill Dental Surgery has more than 100 years of collective experience in providing the best dental care. We owe all our success to the trust put in by you.`,
 };
 
 export default about_long_des;
